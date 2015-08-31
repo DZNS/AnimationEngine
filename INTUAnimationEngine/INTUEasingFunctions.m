5faa7c229412f531d22e48caf108665af9bac220
kyle_resse <akashk@dezinezync.com>
2015-08-31 14:29:28 +0530

    commented out DZEasingFunctios.

--- INTUAnimationEngine/INTUEasingFunctions.m
+++ INTUAnimationEngine/INTUEasingFunctions.m
@@ -284,39 +284,39 @@ INTUEasingFunction INTUEaseInOutBounce = ^CGFloat (CGFloat p) {
     }
 };
 
-DZEasingFunction DZBounceEaseIn = ^CGFloat (CGFloat p)
-{
-    return 1 - DZBounceEaseOut(1 - p);
-};
-
-DZEasingFunction DZBounceEaseOut = ^CGFloat (CGFloat p)
-{
-    if(p < 4/11.0)
-    {
-        return (121 * p * p)/16.0;
-    }
-    else if(p < 8/11.0)
-    {
-        return (363/40.0 * p * p) - (99/10.0 * p) + 17/5.0;
-    }
-    else if(p < 9/10.0)
-    {
-        return (4356/361.0 * p * p) - (35442/1805.0 * p) + 16061/1805.0;
-    }
-    else
-    {
-        return (54/5.0 * p * p) - (513/25.0 * p) + 268/25.0;
-    }
-};
-
-DZEasingFunction DZBounceEaseInOut = ^CGFloat (CGFloat p)
-{
-    if(p < 0.5)
-    {
-        return 0.5 * DZBounceEaseIn(p*2);
-    }
-    else
-    {
-        return 0.5 * DZBounceEaseOut(p * 2 - 1) + 0.5;
-    }
-};
+//DZEasingFunction DZBounceEaseIn = ^CGFloat (CGFloat p)
+//{
+//    return 1 - DZBounceEaseOut(1 - p);
+//};
+//
+//DZEasingFunction DZBounceEaseOut = ^CGFloat (CGFloat p)
+//{
+//    if(p < 4/11.0)
+//    {
+//        return (121 * p * p)/16.0;
+//    }
+//    else if(p < 8/11.0)
+//    {
+//        return (363/40.0 * p * p) - (99/10.0 * p) + 17/5.0;
+//    }
+//    else if(p < 9/10.0)
+//    {
+//        return (4356/361.0 * p * p) - (35442/1805.0 * p) + 16061/1805.0;
+//    }
+//    else
+//    {
+//        return (54/5.0 * p * p) - (513/25.0 * p) + 268/25.0;
+//    }
+//};
+//
+//DZEasingFunction DZBounceEaseInOut = ^CGFloat (CGFloat p)
+//{
+//    if(p < 0.5)
+//    {
+//        return 0.5 * DZBounceEaseIn(p*2);
+//    }
+//    else
+//    {
+//        return 0.5 * DZBounceEaseOut(p * 2 - 1) + 0.5;
+//    }
+//};
